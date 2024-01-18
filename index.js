212fca04ff849d4e68f48c7fd4d62733b8e7cee4
apple <apple@apples-MacBook-Air.local>
2024-01-18 18:46:20 +0100

    new

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('trading'));
 
 const corsOptions = {
-  origin: 'http://localhost:3000', 
+  origin: 'https://earnbroker.com', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
